d37de6d10d1d3990d712d7e758b548a9c2260114
Sam Tebbs <samuel.tebbs@gmail.com>
2025-08-10 10:06:39 +0100

    Remove old as commands from build

--- build.sh
+++ build.sh
@@ -9,8 +9,6 @@ if [ "$debug" = "y" ]
 then
     clang-z80 --target=z80-none-elf -nostdinc $opt -g0 -S -disable-output src/ppu.c -mllvm -print-after-all 2> build/ppu.log
 fi
-#as-z80 -march=z80+full -ignore-undocumented-instructions build/ppu.s -o build/ppu.o
-#as-z80 -march=z80+full -ignore-undocumented-instructions src/maths.s -o build/maths.o
 as-z80 -march=z80+full -ignore-undocumented-instructions src/ppu.s -o build/ppu.o
 # Link the ppu and maths library to an elf file
 ld-z80 -b elf32-z80 $opt -A z80 build/ppu.o -T src/link.ld -o build/ppu.elf
