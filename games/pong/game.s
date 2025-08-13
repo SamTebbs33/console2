28ee3d1b79b60962f587ff014a1553a16656c64b
Sam Tebbs <samuel.tebbs@gmail.com>
2025-08-13 20:32:32 +0100

    Pong now moves bat across the screen

--- games/pong/game.s
+++ games/pong/game.s
@@ -6,18 +6,23 @@ SPRITE_0_ADDR = PPU_DEFS_ADDR + 0 * 64
 .section .intHandler
 .global _intHandler
 _intHandler:
+    inc b
+    jp nz, .ret
     ld ix, SPRITE_TABLE_ADDR
-    ld (ix+2), SPRITE_0_ADDR & 0xFF
-    ld (ix+3), SPRITE_0_ADDR >> 8
-    ld (ix+4), 8
-    ld (ix+5), 8
-    ld (ix+6), SPRITE_0_ADDR & 0xFF
-    ld (ix+7), SPRITE_0_ADDR >> 8
+    ld (ix), c ; x
+    ld (ix+1), 0 ; y
+    ld (ix+2), SPRITE_0_ADDR & 0xFF ; sprite addr low
+    ld (ix+3), SPRITE_0_ADDR >> 8 ; sprite addr high
+    inc c
+.ret:
+    ei
     reti
 
 .section .start
 .global _start
 _start:
+    ld b, 0
+    ld c, 0
     im 1
     ei
 .spin:
