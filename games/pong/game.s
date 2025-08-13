0bed3f0e77b5642ea33a6c920b6a65b9812e1e5a
Sam Tebbs <samuel.tebbs@gmail.com>
2025-08-13 22:59:37 +0100

    Render background in pong

--- games/pong/game.s
+++ games/pong/game.s
@@ -1,7 +1,11 @@
 ; vim: ft=z80 tabstop=4 shiftwidth=4:
+SPRITE_ENTRY_SIZE = 4
+SPRITE_ENTRIES_NUM = 64
 SPRITE_TABLE_ADDR = 48 * 1024
+TILE_TABLE_ADDR = SPRITE_TABLE_ADDR + SPRITE_ENTRIES_NUM * SPRITE_ENTRY_SIZE
 PPU_DEFS_ADDR = 16 * 1024
 SPRITE_0_ADDR = PPU_DEFS_ADDR + 0 * 64
+SPRITE_2_ADDR = PPU_DEFS_ADDR + 2 * 64
 
 .section .intHandler
 .global _intHandler
@@ -20,7 +24,23 @@ _intHandler:
 
 .section .start
 .global _start
+.extern _stack_end
 _start:
+    ld ix, _stack_end
+    ld sp, ix
+    jr setup_background
+
+.section .text
+setup_background:
+    ld ix, TILE_TABLE_ADDR
+    ld bc, 2
+    .rep 19
+        .rep 25
+            ld (ix), SPRITE_2_ADDR & 0xFF
+            ld (ix+1), SPRITE_2_ADDR >> 8
+            add ix, bc
+        .endr
+    .endr
     ld b, 0
     ld c, 0
     im 1
